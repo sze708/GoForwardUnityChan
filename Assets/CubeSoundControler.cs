3db13cca53c18d51b0591ac784bbaa340d816b78
sze <sze.sas1465pt@gmail.com>
2018-12-18 00:42:08 +0900

    課題提出-ブロックが地面とブロックに接触した際に音を鳴らす

---
+++ Assets/CubeSoundControler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSoundControler : MonoBehaviour {
+
+    public AudioClip cubeSound;
+
+    //衝突したオブジェクトのタグがground or Cubeだったら音を1にする。
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+        if (collision.gameObject.tag == "ground")
+        {
+            AudioSource.PlayClipAtPoint(cubeSound, transform.position);
+        }
+
+        if (collision.gameObject.tag == "CubePrefab")
+        {
+            AudioSource.PlayClipAtPoint(cubeSound, transform.position);
+        }
+    }
+
+}
